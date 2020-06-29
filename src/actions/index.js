12d7fd144d4e75f703bf208e5f78e34f31dbd2c0
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-06-28 21:37:49 -0400

    Add middleware redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
 //actions creators
 export const fetchPosts = () => {
-	return{
-		type: 'FETCH_POSTS'
+	return async function (dispatch, getState){
+		const response = await jsonPlaceholder.get('/posts');
+
+		dispatch({type:'FETCH_POSTS', payload: response})
 	};
 };
